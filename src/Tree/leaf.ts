31a7b5318857e3eab8313b07872788c352ab0594
unknown <Mohan.Rajendran@HOUOSP-L-93929.americas.shell.com>
2015-10-10 18:28:41 -0500

    Adde functionality to render cells' color based on type

--- src/Tree/leaf.ts
+++ src/Tree/leaf.ts
@@ -1,40 +1,46 @@
 /// <reference path="tree.ts" />
 
 class Leaf implements Tree {
-	min: Point
-	max: Point
-	
-	constructor(min: Point, max: Point) {
+	min: Point;
+	max: Point;
+	color: string;
+
+	constructor(min: Point, max: Point, color?: string) {
 		var leaf = this;
-		
+
 		leaf.min = min;
 		leaf.max = max;
+		if (color)
+			leaf.color = color;
+		else
+			leaf.color = '#1d831d';
 	}
-	
+
 	render(canvas: HTMLCanvasElement): void {
 		var leaf = this;
-		
-		var height = canvas.height-1;
-		var width = canvas.width-1;
-		
+
+		var height = canvas.height - 1;
+		var width = canvas.width - 1;
+
 		var xMin = Math.round(leaf.min.x * width);
 		var yMin = Math.round(leaf.min.y * height);
 		var xMax = Math.round(leaf.max.x * width);
-		var yMax = Math.round(leaf.max.y * height);	
-		
+		var yMax = Math.round(leaf.max.y * height);
+
 		var ctx = canvas.getContext('2d');
-		ctx.strokeRect(xMin, height-yMax, (xMax - xMin), (yMax - yMin));
+		ctx.strokeStyle = leaf.color;
+		ctx.strokeRect(xMin, height - yMax, (xMax - xMin), (yMax - yMin));
 	}
 }
 
-class Empty extends Leaf {	
+class Empty extends Leaf {
 	constructor(min: Point, max: Point) {
-		super(min, max);
+		super(min, max, '#949494');
 	}
 }
 
 class Full extends Leaf {
 	constructor(min: Point, max: Point) {
-		super(min, max);
+		super(min, max, '#000000');
 	}
 }
